1c06fd596d1658c84113d687db45abf6ebd1f5fc
David Duan <daviduan@amazon.com>
2014-03-06 22:49:18 -0800

    remove un neccesary files

--- LeetCode/LeetCode/containers..cpp
+++
@@ -1,9 +0,0 @@
-//
-//  containers..cpp
-//  LeetCode
-//
-//  Created by Duan, David on 3/4/14.
-//  Copyright (c) 2014 Duan, David. All rights reserved.
-//
-
-#include "containers..h"
--- LeetCode/LeetCode/vectorcontainer.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  vectorcontainer.cpp
-//  LeetCode
-//
-//  Created by Duan, David on 3/4/14.
-//  Copyright (c) 2014 Duan, David. All rights reserved.
-//
-
-#include "vectorcontainer.h"
